19d0e0e723d8973e5db7cb4e75ae80396e6c42a8
dschungeljunge <peter.rigert@gmail.com>
2024-09-15 10:09:37 +0200

    Anbindung Datenbank

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 # SQLAlchemy konfigurieren (Datenbankverbindung)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///datenbank.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dschungeljunge:PHhsluuni8102@junction.proxy.rlwy.net:33275/junction.proxy.rlwy.net'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
